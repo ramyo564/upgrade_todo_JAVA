b1806beca6c2d862ccb5ece058ab425dea440e79
ramyo564 <yohan032yohan@gmail.com>
2025-04-05 21:15:38 +0900

    Jpa 인터페이스 분리 및 TodoRepositoryImpl 구현체 추가

--- src/main/java/com/example/todo/infrastructure/persistence/JpaTodoRepository.java
+++ src/main/java/com/example/todo/infrastructure/persistence/JpaTodoRepository.java
@@ -1,15 +1,12 @@
 package com.example.todo.infrastructure.persistence;
 
 import com.example.todo.domain.model.Todo;
-import com.example.todo.domain.repository.TodoRepository;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface JpaTodoRepository extends JpaRepository<Todo, Long>,
-    TodoRepository {
+import java.util.List;
 
-  @Override
-  List<Todo> findByCompleted(boolean completed);
+@Repository
+public interface JpaTodoRepository extends JpaRepository<Todo, Long> {
+    List<Todo> findByCompleted(boolean completed);
 } 
