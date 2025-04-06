a4fc52e2efe3c8cda8d9033a690c73b66dfc4a8f
ramyo564 <yohan032yohan@gmail.com>
2025-04-07 01:25:19 +0900

    User 레포지토리 -> DDD 아키텍처로 분리

--- src/main/java/com/example/todo/user/infrastructure/persistence/JpaUserRepository.java
+++ src/main/java/com/example/todo/user/infrastructure/persistence/JpaUserRepository.java
@@ -1,7 +1,6 @@
 package com.example.todo.user.infrastructure.persistence;
 
 import com.example.todo.user.domain.model.User;
-import com.example.todo.user.domain.repository.UserRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,11 +10,9 @@ import java.util.Optional;
  * JPA 사용자 저장소 구현체
  */
 @Repository
-public interface JpaUserRepository extends JpaRepository<User, Long>, UserRepository {
+public interface JpaUserRepository extends JpaRepository<User, Long> {
     
-    @Override
     Optional<User> findByEmail(String email);
     
-    @Override
-    Optional<User> findByNickname(String nickname);
+    boolean existsByEmail(String email);
 } 
